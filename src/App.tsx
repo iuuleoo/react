fc10e085da208b6fa7aacdb1f75e1ed5b8bacb6a
Leo <leooproencaaa@gmail.com>
2025-09-29 19:12:12 -0300

    Criando seu próprio Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import "./global.css"
 
 import { Button } from "./components/button"
+import { useMessage } from "./hooks/useMessages"
 
 import styles from "./app.module.css"
 
 export function App(){
+  const { show } = useMessage({ age: 19, name: "Leonardo" })
   return (
   <div className={styles.container}> 
-    <Button name="Adicionar"/>
-    <span>0</span>
-    <Button name="Remover"/> 
-  </div>
+  <Button name="Adicionar" onClick={() => show("Mensagem personalizada do meu hook!")} />
+  <span>0</span>
+  <Button name="Remover" /> 
+</div>
   )
 }
